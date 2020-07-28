c2fa23447a4ac4fdcc821b18bb31ba682eb6c2d2
Rasmus Källvist <rasmus.kallqvist@gmail.com>
2020-07-29 00:13:38 +0200

    Add working example of turning on button LED

--- XoneK2.py
+++ XoneK2.py
@@ -19,45 +19,8 @@ from _Framework.TransportComponent import TransportComponent
 import DebugPrint
 import inspect
 
-EQ_DEVICES = {
-    'Eq8': {
-        'Gains': ['%i Gain A' % (index + 1) for index in range(8)]
-    },
-    'FilterEQ3': {
-        'Gains': ['GainLo', 'GainMid', 'GainHi'],
-        'Cuts': ['LowOn', 'MidOn', 'HighOn']
-    }
-}
-
-# Channels are counted from 0. This is what people would normally call
-# channel 15.
-MIDI_CHANNEL_NUM = 14
-
-NUM_TRACKS = 4
-NUM_SCENES = 4
-
-ENCODERS = [0, 1, 2, 3]
-PUSH_ENCODERS = [52, 53, 54, 55]
-KNOBS1 = [4, 5, 6, 7]
-BUTTONS1 = [48, 49, 50, 51]
-KNOBS2 = [8, 9, 10, 11]
-BUTTONS2 = [44, 45, 46, 47]
-KNOBS3 = [12, 13, 14, 15]
-BUTTONS3 = [40, 41, 42, 43]
-FADERS = [16, 17, 18, 19]
-GRID = [
-    [36, 37, 38, 39],
-    [32, 33, 34, 35],
-    [28, 29, 30, 31],
-    [24, 25, 26, 27],
-]
-ENCODER_LL = 20
-PUSH_ENCODER_LL = 13
-ENCODER_LR = 21
-PUSH_ENCODER_LR = 14
-BUTTON_LL = 12
-BUTTON_LR = 15
-
+# The Xone K2 uses midi channel 15 (zero indexed gives 14)
+MIDI_CHANNEL_NUM = 15 -1
 
 def Button(note_num, name=None):
     rv = ButtonElement(True, MIDI_NOTE_TYPE, MIDI_CHANNEL_NUM, note_num)
@@ -81,12 +44,20 @@ def Encoder(cc):
 
 
 class XoneK2(ControlSurface):
-    def __init__(self, instance):
-        super(XoneK2, self).__init__(instance, False)
+    def __init__(self, c_instance):
+        super(XoneK2, self).__init__(c_instance, False)
         DebugPrint.log_message("XoneK2 constructor called")
 
         with self.component_guard():
             self._set_suppress_rebuild_requests(True)
+
+            # experiment to light up LAYER button red
+            # https://www.allen-heath.com/media/XoneK2_UG_AP8509_3.pdf
+            status = MIDI_NOTE_ON_STATUS + MIDI_CHANNEL_NUM
+            MIDI_NOTE_C0 = 0x0C
+            velocity = 127
+            c_instance.send_midi((status, MIDI_NOTE_C0, velocity))
+
             # self.init_session()
             # self.init_mixer()
             # self.init_matrix()
